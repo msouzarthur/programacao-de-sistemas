3136eb3e9186eaa5af1fd8e0755634b58e4af291
Arthur Souza <amdsouza@inf.ufpel.edu.br>
2022-10-07 11:45:33 -0300

    ajustes no assembler

--- PS/src/Main/Assembler.java
+++ PS/src/Main/Assembler.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import static java.lang.System.exit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -12,6 +13,8 @@ public class Assembler {
 
     private static List<String[]> contentTable = new ArrayList<>();
     private static List<String[]> symbolTable = new ArrayList<>();
+    private static List<String[]> assembledTable = new ArrayList<>();
+
     private static String programName;
 
     public void readContent(String path) {
@@ -27,14 +30,15 @@ public class Assembler {
                 }
                 String[] words = l.split(" ");
                 String[] row = new String[4];
-
-                for (int i = 0; i < words.length; i++) {
-                    row[i] = words[i];
-                    if (words[i].contains("start")) {
-                        programName = words[i + 1];
+                if (!words[0].equals("*")) {
+                    for (int i = 0; i < words.length; i++) {
+                        row[i] = words[i];
+                        if (words[i].contains("start")) {
+                            programName = words[i + 1];
+                        }
                     }
+                    contentTable.add(row);
                 }
-                contentTable.add(row);
             }
             reader.close();
         } catch (FileNotFoundException e) {
@@ -44,6 +48,10 @@ public class Assembler {
             if (r[0].length() == 0) {
                 r[0] = null;
             }
+        }
+        if (!contentTable.get(contentTable.size() - 1)[1].equals("end")) {
+            Error.showError("> diretiva end não encontrada");
+            //exit(0);
         }
         System.out.println("> estrutura do programa " + programName);
         print(contentTable, "|label\tcomando\topd1\topd2\t|");
@@ -101,23 +109,101 @@ public class Assembler {
         return -1;
     }
 
+    //remover coluna de label do codigo montado
+    //ver o opd2 imediato do copy
     void assembleProgram() {
+        int address = 0, start = 0;
         for (String[] r : contentTable) {
-            for (int y = 0; y < r.length; y++) {
-                if (y > 0 && r[y] != null) {
-                    int address = getAddress(r[y]);
-                    if (address != -1) {
-                        r[y] = Integer.toString(address);
+            int a = 0;
+            r[0] = null;
+            address = 0;
+            if (r[1].contains("start")) {
+                a = 1;
+            }
+            if (r[1].contains("stop")) {
+                start = 0;
+                r[1] = Integer.toString(getOpcode(r[1]) + address);
+                assembledTable.add(r);
+            }
+            if (start == 1) {
+                if (r[2] != null && r[2].contains(",I")) {
+                    r[2] = r[2].replace(",I", "");
+                    address += 32;
+                }
+                if (r[3] != null && r[3].contains(",I")) {
+                    r[3] = r[3].replace(",I", "");
+                    address += 64;
+                }
+                if (r[2] != null && r[2].contains("#")) {
+                    r[2] = r[2].replace("#", "");
+                    address += 128;
+                }
+
+                r[1] = Integer.toString(getOpcode(r[1]) + address);
+
+                //simbolos -> codigo
+                for (int i = 2; i < 4; i++) {
+                    int position = getAddress(r[i]);
+                    if (r[i] != null && position != -1) {
+                        r[i] = Integer.toString(position);
+                    } else if (r[i] != null) {
+                        Error.showError("> simbolo não definido " + r[i]);
+                        return;
                     }
                 }
+                assembledTable.add(r);
+            }
+            if (a == 1) {
+                start = 1;
             }
         }
         System.out.println("> programa " + programName + " montado");
-        print(contentTable, "|label\tcomando\topd1\topd2\t|");
+        print(assembledTable, "|label\tcomando\topd1\topd2\t|");
     }
 
-    void toObj() {
-        //implementar
+    int getOpcode(String opd) {
+        opd = opd.trim();
+        switch (opd) {
+            case "br":
+                return 0;
+            case "brpos":
+                return 1;
+            case "add":
+                return 2;
+            case "load":
+                return 3;
+            case "brzero":
+                return 4;
+            case "brneg":
+                return 5;
+            case "sub":
+                return 6;
+            case "store":
+                return 7;
+            case "write":
+                return 8;
+            case "ret":
+                return 9;
+            case "div":
+                return 10;
+            case "stop":
+                return 11;
+            case "read":
+                return 12;
+            case "copy":
+                return 13;
+            case "mult":
+                return 14;
+            case "call":
+                return 15;
+            default:
+                return -1;
+        }
+    }
+
+    void toObj() throws IOException {
+        FileWriter writer = new FileWriter("");
+        writer.close();
     }
 
     void toLst() throws IOException {
@@ -136,6 +222,13 @@ public class Assembler {
             }
             writer.write(System.lineSeparator());
         }
+        writer.write("programa montado" + System.lineSeparator());
+        for (String[] str : assembledTable) {
+            for (String s : str) {
+                writer.write(s + " ");
+            }
+            writer.write(System.lineSeparator());
+        }
         writer.close();
     }
 
@@ -147,13 +240,18 @@ public class Assembler {
         assembleProgram();
         //fim do segundo passo
         System.out.println("> salvando arquivo obj");
-        toObj();
-
+        try {
+            toObj();
+            System.out.println("> arquivo obj salvo");
+        } catch (IOException ex) {
+            System.out.println("> problema ao salvar arquivo obj");
+        }
         System.out.println("> salvando arquivo lst");
         try {
             toLst();
+            System.out.println("> arquivo lst salvo");
         } catch (IOException ex) {
-            System.out.println("Problema ao salvar arquivo");
+            System.out.println("> problema ao salvar arquivo lst");
         }
     }
 }
